2b5fec77cbd02379fad8d3249760b8d6507afe34
AdwinTrave <storyteller@freedombase.net>
2014-07-12 19:14:05 +0200

    JS sign_up validation is back + can now set min and max username length and min password length

--- application/third_party/Hybrid/Auth.php
+++ application/third_party/Hybrid/Auth.php
@@ -14,7 +14,7 @@
  */
 class Hybrid_Auth 
 {
-	public static $version = "2.2.2-dev";
+	public static $version = "2.3.0-dev";
 
 	public static $config  = array();
 
@@ -82,9 +82,9 @@ class Hybrid_Auth
 		require_once $config["path_base"] . "User_Contact.php";
 		require_once $config["path_base"] . "User_Activity.php";
 
-        if(!class_exists("Hybrid_Storage")){
-            require_once $config["path_base"] . "Storage.php";
-        }
+		if ( ! class_exists("Hybrid_Storage", false) ){
+			require_once $config["path_base"] . "Storage.php";
+        	}
 
 		// hash given config
 		Hybrid_Auth::$config = $config;
